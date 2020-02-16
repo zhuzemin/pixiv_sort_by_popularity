d3d33f7b65fe67508b192ee174eecb04e67c7e2d
asd <zhuzemin4@hotmail.com>
2020-02-17 07:19:47 +0800

    script complete

---
+++ README.md
@@ -0,0 +1,2 @@
+![effect](https://github.com/zhuzemin/pixiv_sort_by_popularity/raw/master/2020-02-17_071703.jpg)
+
--- pixiv_sort_by_popularity.user.js
+++ pixiv_sort_by_popularity.user.js
@@ -23,22 +23,22 @@
 // @connect-src workers.dev
 // ==/UserScript==
 var config = {
-  'debug': true
+  'debug': false
 }
 var debug = config.debug ? console.log.bind(console)  : function () {
 };
 
 //this userscript desire for free member use "Sort by popularity"
 
-
-//pixiv search request through this url will use my cookie.(I'm not premium...)
+//default
+//pixiv search request through this url will use my cookie.
 var cloudFlareUrl='https://proud-surf-e590.zhuzemin.workers.dev/ajax/';
 
 //Obejct use for xmlHttpRequest
 class requestObject{
-    constructor(keyword) {
+    constructor(keyword,page,order,mode) {
         this.method = 'GET';
-        this.url = cloudFlareUrl+'https://www.pixiv.net/ajax/search/artworks/'+keyword+'?word='+keyword+'&order=date&mode=all&p=1&s_mode=s_tag&type=all';
+        this.url = cloudFlareUrl+'https://www.pixiv.net/ajax/search/artworks/'+keyword+'?word='+keyword+'&order='+order+'&mode='+mode+'&p='+page+'&s_mode=s_tag&type=all';
         this.data=null,
             this.headers = {
                 'User-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
@@ -51,13 +51,16 @@ class requestObject{
     }
 }
 
+var btn;
+var page;
+var select;
 
 // prepare UserPrefs
 setUserPref(
     'cloudFlareUrl',
     cloudFlareUrl,
     'Set cloudFlareUrl',
-    `cloudFlareUrl only working for "Sort by popularity"`,
+    `cloudFlareUrl only working on "Sort by popularity"`,
     ','
 );
 
@@ -76,43 +79,40 @@ function addJS_Node (text)
 
 
 //override fetch
-function intercept(responseDetails){
-    //response from my server
-    var newData=responseDetails.response;
-    debug(newData);
-    //insert override function to page, include response
+function intercept(){
+    //insert override function to page
     addJS_Node(`
-const constantMock = window.fetch;
- window.fetch = function() {
-  console.log(arguments);
+    var newData;
+    var interceptEnable;
+    var constantMock = window.fetch;
+    window.fetch = function() {
+        console.log(arguments);
 
     return new Promise((resolve, reject) => {
         constantMock.apply(this, arguments)
             .then((response) => {
-                if(response.url.includes('/ajax/search/artworks/')&&response){
-                    console.log(response);
-var blob = new Blob([JSON.stringify(`+JSON.stringify(newData)+`,null,2)], {type : 'application/json'});
+    if(interceptEnable&&/https:\\/\\/www\\.pixiv\\.net\\/ajax\\/search\\/artworks\\/[^\\?&]*\\?word=[^\\?&]*&order=date(_d)?/.test(response.url)){
+       var blob = new Blob([newData], {type : 'application/json'});
+         console.log(newData);
 
-                    var newResponse=new Response(
+        var newResponse=new Response(
         blob, {
         status: response.status,
         statusText: response.statusText,
         headers: response.headers
       });
-                    console.log(newResponse);
-                }
-                resolve(newResponse);
+        console.log(newResponse);
+                    response=newResponse;
+                    interceptEnable=false;
+    }
+                resolve(response);
             })
             .catch((error) => {
                 reject(response);
             })
     });
  }
- `);
-    //trigger fetch by click "Newest" or "Oldest"
-    var div=document.querySelectorAll('div.sc-LzMhL.krKUte')[1];
-    div.querySelector('a').click();
-
+         `);
     //here is script end,
     //in console ,log show fetch response body has been changed <--- not very sure
     //and page have react ---> stay blank for ever
@@ -127,27 +127,116 @@ var blob = new Blob([JSON.stringify(`+JSON.stringify(newData)+`,null,2)], {type 
 
 //userscript entry
 var init=function(){
-        debug("init");
         //create button
-    var div=document.querySelectorAll('div.sc-LzMhL.krKUte')[1];
-    var btn =document.createElement('button');
-    btn.innerHTML='Sort by popularity';
+    if(window.self === window.top){
+        debug("init");
+        cloudFlareUrl=GM_getValue('cloudFlareUrl')||cloudFlareUrl;
+        intercept();
+        var div=document.querySelectorAll('div.sc-LzMhL.krKUte')[1];
+        btn =document.createElement('button');
+        btn.innerHTML='Sort by popularity';
         btn.addEventListener('click',sortByPopularity);
-    div.insertBefore(btn,null);
+        div.insertBefore(btn,null);
+        select=document.createElement('select');
+        select.id='sortByPopularity';
+        var optionObj={
+            'Popular with all':'popular_d',
+            'Popular (male)':'popular_male_d',
+            'Popular (female)':'popular_female_d'
+        }
+        for(var key of Object.keys(optionObj)){
+            var option=document.createElement('option');
+            option.innerHTML=key;
+            option.value=optionObj[key];
+            select.insertBefore(option,null);
+        }
+        div.insertBefore(select,null);
+    }
 }
 
 window.addEventListener('load', init);
 
 //get current search word, then use xmlHttpRequest get response(from my server)
-function sortByPopularity() {
-    var keyword=window.location.href.match(/https:\/\/www\.pixiv\.net\/(\w*\/)?tags\/(.*)\//)[2];
-    debug(keyword);
-    var obj=new requestObject(keyword);
-    request(obj,intercept);
+function sortByPopularity(e) {
+    btn.innerHTML='Searching...'
+    var keyword;
+        var matching=window.location.href.match(/https:\/\/www\.pixiv\.net\/(\w*\/)?tags\/(.*)\/artworks\?(order=[^\?&]*)?&?(mode=(\w\d*))?&?(p=(\d*))?/);
+        keyword=matching[2]
+    debug(e.target.tagName)
+    if(/(\d*)/.test(e.target.textContent)&&(e.target.tagName=='SPAN'||e.target.tagName=="A")){
+        page=e.target.textContent.match(/(\d*)/)[1];
+    }
+    else if(e.target.tagName=='svg'||e.target.tagName=='polyline'){
+            if(e.target.parentElement.tagName=='a'){
+                page=e.target.parentElement.href.match(/p=(\d*)/)[1];
+
+            }
+            else {
+                page=e.target.parentElement.parentElement.href.match(/p=(\d*)/)[1];
+
+            }
+    }
+        else if(/p=(\d*)/.test(window.location.href)){
+            page=window.location.href.match(/p=(\d*)/)[1];
+
+        }
+        else{
+            page=1;
+    }
+    page=parseInt(page);
+        debug(keyword);
+        debug('page: '+page);
+        var order=document.querySelector('#sortByPopularity').value;
+        var mode;
+        if(/(&|\?)mode=([\d\w]*)/.test(window.location.href)){
+            mode=window.location.href.match(/(&|\?)mode=([\d\w]*)/)[2];
+        }
+        else {
+            mode='all';
+        }
+        var obj=new requestObject(keyword,page,order,mode);
+        request(obj,function (responseDetails) {
+
+            unsafeWindow.newData=JSON.stringify(responseDetails.response,null,2);
+            unsafeWindow.interceptEnable=true;
+            //trigger fetch by click "Newest" or "Oldest"
+            var div=document.querySelectorAll('div.sc-LzMhL.krKUte')[1];
+            div.querySelector('a').click();
+            var interval=setInterval(function () {
+                var nav=document.querySelector('nav.sc-LzNRw.qhAyw');
+                if(nav!=null){
+                    nav.addEventListener('click',sortByPopularity);
+                    nav.childNodes[1].childNodes[0].innerText=page;
+                    if(page<=7&&page>1){
+                        nav.childNodes[1].href=nav.childNodes[page].href;
+                        nav.childNodes[page].innerText=1;
+                        nav.childNodes[page].href=nav.childNodes[0].href;
+                        nav.insertBefore(nav.childNodes[1],nav.childNodes[page]);
+                        nav.insertBefore(nav.childNodes[page],nav.childNodes[1]);
+
+                    }
+                    else if(page>7){
+                        //nav.insertBefore(nav.childNodes[1],nav.childNodes[8]);
+                        for(var i=2;i<8;i++){
+                            nav.childNodes[i].childNodes[0].innerText=page+i-1;
+                            nav.childNodes[i].href=nav.childNodes[i].href.replace(/p=\d*/,'p='+(page+i-1));
+                        }
+                    }
+                    if(page!=1){
+                        nav.childNodes[0].style='opacity:1!important;';
+                        nav.childNodes[0].href=nav.childNodes[8].href.replace(/p=\d*/,'p='+(page-1));
+                        nav.childNodes[8].href=nav.childNodes[8].href.replace(/p=\d*/,'p='+(page+1));
+
+                    }
+                    btn.innerHTML='Sort by popularity';
+                    clearInterval(interval);
+
+                }
+            },2000);
+        });
 
 }
 function request(object,func) {
-    var retries = 3;
     GM_xmlhttpRequest({
         method: object.method,
         url: object.url,
@@ -157,24 +246,11 @@ function request(object,func) {
         timeout: 60000,
         //synchronous: true
         onload: function (responseDetails) {
-            if (responseDetails.status != 200) {
-                // retry
-                if (retries--) {          // *** Recurse if we still have retries
-                    setTimeout(request(),2000);
-                    return;
-                }
-            }
             debug(responseDetails);
             //Dowork
             func(responseDetails);
         },
         ontimeout: function (responseDetails) {
-            debug(responseDetails);
-            // retry
-            if (retries--) {          // *** Recurse if we still have retries
-                setTimeout(request,2000);
-                return;
-            }
             //Dowork
             func(responseDetails);
 
